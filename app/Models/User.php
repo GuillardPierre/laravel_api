72c6a5e7edd9e944b855fe4621773b24cd6ce4c4
PierreGllrd <pguillard95@gmail.com>
2026-05-22 11:41:40 +0200

    Add method to check for professional email and corresponding unit tests

--- app/Models/User.php
+++ app/Models/User.php
@@ -30,4 +30,12 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function usesProfessionalEmail(): bool
+    {
+        return !preg_match(
+            '/@(gmail|yahoo|hotmail|live|outlook)\./',
+            $this->email,
+        );
+    }
 }
